4bef32edecbb4a410870261aeb8115ce6b6b8d39
luisrojasgh <luisvillegastlx@gmail.com>
2023-11-10 15:51:04 -0500

    Actualización en views

--- base/views.py
+++ base/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-#from configuracion.models import Slider
+from configuracion.models import Slider
 
 def principal(request):
     titulo="Bienvenido"
-    #sliders= Slider.objects.filter(estado=True)
+    sliders= Slider.objects.filter(estado=True)
     context={
         "titulo": titulo,
-        #"sliders": sliders
+        "sliders": sliders
     }
     return render(request, "index.html", context)
 
